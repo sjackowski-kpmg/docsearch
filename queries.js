d1ec63fbb64f51eb059304b8e9009b0b5ef93526
Steven Jackowski <sjackowski@kpmg.com>
2017-05-21 13:44:08 -0700

    Updating the schema and table name in the search query.

--- queries.js
+++ queries.js
@@ -7,14 +7,6 @@ var options = {
 var dbconfig = require('./dbconfig.json');
 
 var pgp = require('pg-promise')(options);
-// var connectionString = 'postgres://stevenjackowski@localhost:5432/stevenjackowski';
-// var cn = {
-//   host: 'localhost',
-//   port: 5432,
-//   database: 'stevenjackowski',
-//   user: 'stevenjackowski'
-// };
-
 var db = pgp(dbconfig);
 
 function searchResumes(req, res, next) {
@@ -26,7 +18,7 @@ function searchResumes(req, res, next) {
   (
   select id, ts_rank_cd(to_tsvector('english', text_data), $1) as score,
     to_tsvector('english', text_data) @@ to_tsquery('english', $1) as term_found
-  from docstore.resume
+  from docs.resumes
   order by score desc
   ) q where q.term_found = TRUE;`;
 
